e4a46457277eb55b8d7de3c61c57f65e3b3d3ba6
DDKFM <maxschaedlich@hotmail.de>
2019-11-23 08:26:22 +0100

    remote the meatureTime method

--- src/main/kotlin/de/ddkfm/StickerStorage/controller/AuthenticationController.kt
+++ src/main/kotlin/de/ddkfm/StickerStorage/controller/AuthenticationController.kt
@@ -3,7 +3,6 @@ package de.ddkfm.StickerStorage.controller
 import de.ddkfm.StickerStorage.models.Token
 import de.ddkfm.StickerStorage.security.JwtTokenProvider
 import de.ddkfm.StickerStorage.security.SecurityConstants
-import de.ddkfm.StickerStorage.utils.measureTime
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
@@ -23,22 +22,20 @@ import javax.servlet.http.HttpServletRequest
 class AuthenticationController {
     @GetMapping("")
     fun login(request : HttpServletRequest): ResponseEntity<Token> {
-        return measureTime("login") {
-            val forbidden = ResponseEntity.status(HttpStatus.FORBIDDEN).build<Token>()
-            val auth = request.getHeader("Authorization")
-                    ?: return@measureTime  forbidden
-            val encoded = Base64
-                    .getDecoder()
-                    .decode(auth.replace("Basic ", "").toByteArray())
-                    ?.let { String(it) }
-                    ?: return@measureTime forbidden
-            if(!encoded.contains(":"))
-                return@measureTime forbidden
-            val username = encoded.split(":")[0]
-            val password = encoded.split(":")[1]
-            if(username != SecurityConstants.AUTHENTICATION_USERNAME || password != SecurityConstants.AUTHENTICATION_PASSWORD)
-                return@measureTime forbidden
-            ok(JwtTokenProvider.createToken(username))
-        }
+        val forbidden = ResponseEntity.status(HttpStatus.FORBIDDEN).build<Token>()
+        val auth = request.getHeader("Authorization")
+                ?: return forbidden
+        val encoded = Base64
+                .getDecoder()
+                .decode(auth.replace("Basic ", "").toByteArray())
+                ?.let { String(it) }
+                ?: return forbidden
+        if(!encoded.contains(":"))
+            return forbidden
+        val username = encoded.split(":")[0]
+        val password = encoded.split(":")[1]
+        if(username != SecurityConstants.AUTHENTICATION_USERNAME || password != SecurityConstants.AUTHENTICATION_PASSWORD)
+            return forbidden
+        return ok(JwtTokenProvider.createToken(username))
     }
 }
--- src/main/kotlin/de/ddkfm/StickerStorage/utils/ResponseUtils.kt
+++ src/main/kotlin/de/ddkfm/StickerStorage/utils/ResponseUtils.kt
@@ -15,18 +15,6 @@ fun HttpServletRequest.location(path : String) : UriComponentsBuilder {
 fun UriComponentsBuilder.withParams(vararg params : Any?) : UriComponents {
     return this.buildAndExpand(*params)
 }
-
-fun UriComponentsBuilder.withParams(params : Map<String, Any?>) : UriComponents {
-    return this.buildAndExpand(params)
-}
-
 fun UriComponents.created() : ResponseEntity.BodyBuilder {
     return ResponseEntity.created(this.toUri())
-}
-
-fun <T> measureTime(name : String, builder : () -> T) : T {
-    val start = System.currentTimeMillis()
-    val result = builder()
-    println("time for $name: ${System.currentTimeMillis() - start} ms")
-    return result
 }
