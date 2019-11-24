a732eb0b1ccc445d6010177ffcfdf96c81519c90
DDKFM <maxschaedlich@hotmail.de>
2019-11-24 08:47:58 +0100

    add update endpoint for locations

--- src/main/kotlin/de/ddkfm/StickerStorage/controller/LocationController.kt
+++ src/main/kotlin/de/ddkfm/StickerStorage/controller/LocationController.kt
@@ -4,19 +4,14 @@ import de.ddkfm.StickerStorage.models.Location
 import de.ddkfm.StickerStorage.repository.LocationRepository
 import de.ddkfm.StickerStorage.utils.created
 import de.ddkfm.StickerStorage.utils.location
+import de.ddkfm.StickerStorage.utils.saveIn
 import de.ddkfm.StickerStorage.utils.withParams
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.*
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.PostMapping
-
 
 
 @RestController
@@ -44,10 +39,26 @@ class LocationController(private val locations: LocationRepository) {
     fun create(@RequestBody location: Location, request: HttpServletRequest): ResponseEntity<Location> {
         val saved = this.locations.save(location)
         return request
-                .location("/v1/location/{id}")
+                .location("/v1/locations/{id}")
                 .withParams(saved.id)
                 .created()
                 .body(saved)
     }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable("id") id : Long, @RequestBody location : Location, request: HttpServletRequest) : ResponseEntity<Location> {
+        val existing = locations.findById(id).orElseGet(null)
+                ?: return notFound().build()
+        val newLocation = existing.apply {
+            this.name = location.name
+            this.comment = location.comment
+        }.saveIn(locations)
+        return request
+                .location("/v1/locations/{id}")
+                .withParams(newLocation?.id)
+                .created()
+                .body(newLocation)
+    }
+
 
 }
